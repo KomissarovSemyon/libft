817e0ad24564654bd184bdc517ef265708e4d746
Komissarov Semyon <komissarov.semyon@gmail.com>
2018-12-05 11:04:41 +0300

    Fixed atoi long long overflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: semenkomissarov <semenkomissarov@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 12:39:30 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/11/29 20:48:35 by amerlon-         ###   ########.fr       */
+/*   Updated: 2018/12/05 11:02:30 by semenkomiss      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,15 @@ int	ft_atoi(const char *str)
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r')
 		i++;
-	if (str[i] == '-')
-		sign = -1;
-	if (str[i] == '+')
-		sign = 1;
+	str[i] == '-' ? sign = -1 : (0);
+	str[i] == '+' ? sign = 1 : (0);
 	str[i] == '+' || str[i] == '-' ? i++ : (0);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		prev = res;
 		res = res * 10 + str[i] - '0';
-		if ((prev > 0 && res < 0) || (prev < 0 && res > 0))
+		if ((prev > 0 && res < 0) || (prev < 0 && res > 0) ||
+			(prev % 10 != res / 10 % 10))
 			return (sign > 0 ? -1 : 0);
 		i++;
 	}
