0c6ac2f5d4d06e89132828f6eff6da97f140fefe
amerlon- <amerlon-@student.21-school.ru>
2018-12-06 14:55:19 +0300

    fixed realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 19:55:34 by amerlon-          #+#    #+#             */
-/*   Updated: 2018/12/04 20:06:14 by amerlon-         ###   ########.fr       */
+/*   Updated: 2018/12/06 14:53:35 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	*ft_realloc(void *ptr, size_t size, size_t new_size)
 
 	res = ft_memalloc(new_size);
 	res = ft_memmove(res, ptr, size > new_size ? new_size : size);
+	free(ptr);
 	return (res);
 }
